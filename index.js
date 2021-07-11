5d33dd65312560d3eac887137179480d20e7cbb8
Natalie Meurer <natalie.a.meurer@gmail.com>
2021-07-11 15:51:48 -0500

    Fix routing.

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'packages', 'react-app', 'build')));
 
-app.get('/', function (req, res) {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'packages', 'react-app', 'build', 'index.html'));
 });
 
 app.listen(process.env.PORT || 9000);
 
-console.log("Listening on port :" + process.env.PORT || 9000);
+console.log("Listening on port :" + (process.env.PORT || 9000));
